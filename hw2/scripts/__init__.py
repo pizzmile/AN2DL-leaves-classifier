51b5dde2378d8296e23d22e15d610f6b0e4fa49b
pizzmile <giacomo.pizza99@gmail.com>
2021-12-21 18:21:28 +0100

    split

--- hw2/scripts/__init__.py
+++
@@ -1,3 +0,0 @@
-from .constructors import *
-from .custom_logger import *
-from .safe_directories import createDirectoryTree
--- hw2/scripts/constructors/__init__.py
+++
@@ -1,3 +0,0 @@
-from .join import join_path, join
-from .math import divide
-from .conversions import to_float, to_tuple, map_to_list
